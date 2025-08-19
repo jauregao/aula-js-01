46915cf8c8e43029a1b970686824badc5cfb7797
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-08-19 15:22:29 -0300

    editando exercicio while

--- while.js
+++ while.js
@@ -4,27 +4,6 @@
 
   // let soma = 0;
 
-  // // enquanto (isso é verdade) {
-  // // faça isso
-  // // incremento
-  // //}
-  // while (i <= 100) {
-  //   soma += i;
-  //   //console.log("[SOMA]", soma);
-  //   //console.log("[ITERADOR]", i);
-  //   i++;
-  //   //console.log("[ITERADOR + 1]", i); 
-  // }
-  
-  // console.log("Soma total:", soma);
-
-
-
-  // Contar de 10 a 1 (decrescente)
-  // define variavel como 10
-  // enquanto a variavel for maior que 1 repita
-  // contador-- === contador = contador - 1
-
   let contador = 10;
 
   while(contador >= 1){
